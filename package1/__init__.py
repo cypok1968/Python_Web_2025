10bbfc4aee2f3982ecb453cd1df7c8772f5344eb
cypok1968 <cypku1968@gmail.com>
2025-07-08 02:07:43 +0300

    Update __init__.py

--- package1/__init__.py
+++ package1/__init__.py
@@ -1,8 +1,9 @@
 # __init__.py
 from .module import greet
+from.module import factorial
 from .utils import add
 
 __version__ = '1.0.0'
 __doc__ = 'Это пакет, который содержит...'
 __author__ = 'John'
-__all__ = ['greet', 'add']  # для импорта со *
+__all__ = ['greet', 'add', 'factorial']  # для импорта со *
